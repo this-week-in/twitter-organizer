7a99f6eb4ac0a02b5f255d85e145c565d91e186c
Josh Long <josh@joshlong.com>
2020-04-06 01:24:57 -0700

    /Users/jlong/code/this-week-in/twitter-organizer-job

--- src/main/kotlin/com/joshlong/twitter/organizer/TwitterOrganizer.kt
+++ src/main/kotlin/com/joshlong/twitter/organizer/TwitterOrganizer.kt
@@ -2,13 +2,9 @@ package com.joshlong.twitter.organizer
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.event.EventListener
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
-import org.springframework.stereotype.Component
 import javax.sql.DataSource
 
 /**
@@ -25,22 +21,4 @@ class TwitterOrganizer {
 	@Bean
 	fun jdbcTemplate(ds: DataSource) = JdbcTemplate(ds)
 
-}
-
-@Component
-class Initializer(private val jdbcTemplate: JdbcTemplate) {
-
-	@EventListener(ApplicationReadyEvent::class)
-	fun ready() {
-
-		data class Reservation(val id: Long, val name: String)
-
-		this.jdbcTemplate
-				.queryForList("select * from reservation", RowMapper<Reservation> { rs, i ->
-					Reservation(rs.getLong("id"), rs.getString("name"))
-				})
-				.forEach {
-					println(it)
-				}
-	}
 }
